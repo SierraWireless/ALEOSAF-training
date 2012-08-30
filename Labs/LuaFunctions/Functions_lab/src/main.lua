4ed60584779a6eddb3503a1c5164d02e13183ea1
Gaetan Morice <gaetan.morice@gmail.com>
2012-08-30 15:57:40 +0200

    Refactor lab and solution for Lua Function Lab

--- Labs/LuaFunctions/Functions_lab/src/main.lua
+++ Labs/LuaFunctions/Functions_lab/src/main.lua
@@ -1,54 +1,116 @@
+-- ============================================================================
+-- Labs Lua Function.
+-- ============================================================================
+
+-- ----------------------------------------------------------------------------
+-- Includes section
+-- ----------------------------------------------------------------------------
+
 local sched = require 'sched'
 
--- Step.1
--- Create a local function that just print "hello".
--- Call the created function from the main and run it to test
+-- ----------------------------------------------------------------------------
+-- TODO Step.1: Print a string.
+-- ----------------------------------------------------------------------------
+-- 1. Create a function that prints "hello".
+-- 2. Call the function from the main function. 
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S1_print_hello
+
+-- TODO Step.1.1: create function S1_print_hello
+
+-- ----------------------------------------------------------------------------
+-- TODO Step.2: Print some maths.
+-- ----------------------------------------------------------------------------
+-- 1. Create a local function that takes two arguments and prints the result
+--    the division of the first ons by the second one.
+-- 2. Call the function from the main function. 
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S2_print_divide
+-- @param #number dividend
+-- @param #number divisor 
+
+-- TODO Step.2.1: create function S2_print_divide
+
+-- ----------------------------------------------------------------------------
+-- TODO Step.3: Return a value.
+-- ----------------------------------------------------------------------------
+-- 1. Create a local function that also divides its two arguments 
+--    but returns the relust instead of printing it.
+-- 2. Call the function and print the result in the main function.
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S3_divide
+-- @param #number dividend
+-- @param #number divisor 
+-- @return #number quotient
+
+-- TODO Step.3.1: create function S3_divide
+
+-- ----------------------------------------------------------------------------
+-- TODO Step.4: Return multiple values.
+-- ----------------------------------------------------------------------------
+-- 1. Create a local function that performs an euclidean division of its two
+--    arguments and returns both the quotient and the remainder:
+--     * To compute the qotient, use math.floor(number) 
+--       that returns the interger of a number.
+--     * To compute the remainder, use the % operator to perform modulo.
+-- 2. Call this function and print both returned values in the main function.
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S4_euclidean_divide
+-- @param #number dividend
+-- @param #number divisor 
+-- @return #number, #number quotient and remainder
+
+-- TODO Step.4.1: create function S4_euclidean_divide
 
 
--- Step.2
--- Create a local function that takes two numbers arguments and 
--- print the result of the first arguement divided by the second one
+-- ----------------------------------------------------------------------------
+-- TODO Step.5: Handle errors.
+-- ----------------------------------------------------------------------------
+-- 1. Create a local function that also divides its two arguments and handles
+--    the divide by zero error:
+--    * If the divisor is return two values: nil and an error message
+--    * Otherwise return the result of the division.
+-- 2. Call this function and handle the error case result 
+--    by using "assert" function in the main function.
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
 
+--- @function [parent=#global] S5_error_divide
+-- @param #number dividend
+-- @param #number divisor
+-- @return #number quotient
+-- @return #nil, #string error message
 
--- Step.3
--- Same function that divide two numbers but just return the result instead of print it
--- Call the function and print the result in the main.
+-- TODO Step.5.1: create function S4_euclidean_divide
 
-
--- Step.4
--- Create a function that perform an euclidian divide and return two params the result and the rest
--- To compute the result, use math.floor(number) that return the interger of a number
--- To compute the rest, use the % operator to perform modulo
--- Call this function in the main and print both returned values
-
-
--- Step.5
--- Same function but handle the divide by zero error by returning a nil values followed by an error message
--- If there are no error, just return the result as usual
--- Call this function in the main and handle the error case result by using "assert" function
-
+-- ----------------------------------------------------------------------------
+-- main function
+-- ----------------------------------------------------------------------------
 
 local function main ()
 
-	-- Put call to local function define above here
-
-
-	-- Step.6
-	-- Affect a divide function in a local variable
-
-
-	-- Step.7
-	-- Create two functions: 
-	-- The first one will take a parameter as parameter and print it.
-	-- The second one will take two numbers and the first callback function as parameters 
-	-- and will divide the numbers and call the callback function with the result
-	-- And the end, call the divide function with the callback function as parameter
+	-- Write here the calls to the local functions defined in previous steps.
+	-- TODO Step.1.2: call function S1_print_hello
+	-- TODO Step.2.2: call function S2_print_divide
+	-- TODO Step.3.2: call function S3_divide
+	-- TODO Step.4.2: call function S4_euclidean_divide
+	-- TODO Step.5.2: call function S5_error_divide with assert
 	
-	
-	-- The end
 	print("end")
 	os.exit()
 end
+
+-- ----------------------------------------------------------------------------
+-- bootstrap code
+-- ----------------------------------------------------------------------------
 
 sched.run(main)
 sched.loop()
--- Labs/LuaFunctions/Functions_solution/src/main.lua
+++ Labs/LuaFunctions/Functions_solution/src/main.lua
@@ -1,118 +1,130 @@
+-- ============================================================================
+-- Labs Lua Function.
+-- ============================================================================
+
+-- ----------------------------------------------------------------------------
+-- Includes section
+-- ----------------------------------------------------------------------------
+
 local sched = require 'sched'
 
--- Step.1
--- Create a local function that just print "hello".
--- Call the created function from the main and run it to test
-local function printhello ()
-	print ('hello')
+-- ----------------------------------------------------------------------------
+-- TODO Step.1: Print a string.
+-- ----------------------------------------------------------------------------
+-- 1. Create a function that prints "hello".
+-- 2. Call the function from the main function. 
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S1_print_hello
+
+function S1_print_hello()
+	print('hello')
 end
 
--- Step.2
--- Create a local function that takes two numbers arguments and 
--- print the result of the first arguement divided by the second one
-local function divide3(arg1, arg2)
-	print ("result:"..arg1/arg2)
+-- ----------------------------------------------------------------------------
+-- TODO Step.2: Print some maths.
+-- ----------------------------------------------------------------------------
+-- 1. Create a local function that takes two arguments and prints the result
+--    the division of the first ons by the second one.
+-- 2. Call the function from the main function. 
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S2_print_divide
+-- @param #number dividend
+-- @param #number divisor 
+
+function S2_print_divide(dividend, divisor)
+	print(dividend/divisor)
 end
 
--- Step.3
--- Same function that divide two numbers but just return the result instead of print it
--- Call the function and print the result in the main.
-local function divide4(arg1, arg2)
-	return arg1/arg2
+
+-- ----------------------------------------------------------------------------
+-- TODO Step.3: Return a value.
+-- ----------------------------------------------------------------------------
+-- 1. Create a local function that also divides its two arguments 
+--    but returns the relust instead of printing it.
+-- 2. Call the function and print the result in the main function.
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S3_divide
+-- @param #number dividend
+-- @param #number divisor 
+-- @return #number quotient
+
+function S3_divide(dividend, divisor)
+	return dividend/divisor
 end
 
--- Step.4
--- Create a function that perform an euclidian divide and return two params the result and the rest
--- To compute the result, use math.floor(number) that return the interger of a number
--- To compute the rest, use the % operator to perform modulo
--- Call this function in the main and print both returned values
-local function divide6(arg1, arg2)
-	local result = math.floor(arg1/arg2)
-	local rest = arg1%arg2
-	return result, rest
+-- ----------------------------------------------------------------------------
+-- TODO Step.4: Return multiple values.
+-- ----------------------------------------------------------------------------
+-- 1. Create a local function that performs an euclidean division of its two
+--    arguments and returns both the quotient and the remainder:
+--     * To compute the qotient, use math.floor(number) 
+--       that returns the interger of a number.
+--     * To compute the remainder, use the % operator to perform modulo.
+-- 2. Call this function and print both returned values in the main function.
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S4_euclidean_divide
+-- @param #number dividend
+-- @param #number divisor 
+-- @return #number, #number quotient and remainder
+
+function S4_euclidean_divide(dividend, divisor)
+	return math.floor(dividend/divisor), dividend%divisor
 end
 
--- Step.6
--- Same function but handle the divide by zero error by returning a nil values followed by an error message
--- If there are no error, just return the result as usual
--- Call this function in the main and handle the error case result by using "assert" function
-local function divide7(arg1, arg2)
-	if (arg2 == 0) then
-		return nil, "Divide by zero is forbidden"
+-- ----------------------------------------------------------------------------
+-- TODO Step.5: Handle errors.
+-- ----------------------------------------------------------------------------
+-- 1. Create a local function that also divides its two arguments and handles
+--    the divide by zero error:
+--    * If the divisor is return two values: nil and an error message
+--    * Otherwise return the result of the division.
+-- 2. Call this function and handle the error case result 
+--    by using "assert" function in the main function.
+-- 3. Run the project.
+-- ----------------------------------------------------------------------------
+
+--- @function [parent=#global] S5_error_divide
+-- @param #number dividend
+-- @param #number divisor
+-- @return #number quotient
+-- @return #nil, #string error message
+
+function S5_error_divide(dividend, divisor)
+	if divisor == 0 
+		then return nil, "Attempt to divide by 0"
+		else return dividend/divisor
 	end
-	local result = math.floor(arg1/arg2)
-	local rest = arg1%arg2
-	return result, rest
 end
 
+-- ----------------------------------------------------------------------------
+-- main function
+-- ----------------------------------------------------------------------------
 
 local function main ()
 
-	-- Put call to local function define above here
-	-- Call the print hello function
-	printhello()
+	-- Write here the calls to the local functions defined in previous steps.
+	S1_print_hello()
+	S2_print_divide(11,2)
+	print(S3_divide(11,2))
+	print(S4_euclidean_divide(11,2))
+	print(assert(S5_error_divide(11,2))); print(assert(S5_error_divide(11,0)))
 	
-	-- Call the divide function
-	divide3(11,2)
-	
-	-- Handle a function that return a value
-	local result4 = divide4(11, 2)
-	print ("result:"..result4)
-	
-	-- Handle multiple values on function return
-	local result6, rest6 = divide6(11,2)
-	print ("result:"..result6..",rest:"..rest6)
-	
-	-- A successful divide
-	local result7, rest7 = assert(divide7(11,2))
-	print ("result:"..result7..",rest:"..rest7)
-	
-	-- A failling divide
-	-- result7, rest7 = assert(divide7(11,0))
-	print ("result:"..result7..",rest:"..rest7)
-
-	-- Step.7
-	-- Affect a divide function in a local variable
-	local divide9 = function (arg1, arg2)
-		if (arg2 == 0) then
-			return nil, "Divide by zero is forbidden"
-		end
-		local result = math.floor(arg1/arg2)
-		local rest = arg1%arg2
-		return result, rest
-	end
-	-- Add call the function from the variable
-	local result9, rest9 = assert(divide9(11,2))
-	print ("result:"..result9..",rest:"..rest9)
-
-	-- Step.8
-	-- Create two functions: 
-	-- The first one will take a parameter as parameter and print it.
-	-- The second one will take two numbers and the first callback function as parameters 
-	-- and will divide the numbers and call the callback function with the result
-	-- And the end, call the divide function with the callback function as parameter
-	
-	-- Callback function
-	local handleresultfn =  function (result, rest)
-		print ("result:"..result..",rest:"..rest)
-	end
-	-- Divide function
-	local divide2fn = function (arg1, arg2, handleresult)
-		if (arg2 == 0) then
-			print ("Divide by zero is forbidden")
-		end
-		local result = math.floor(arg1/arg2)
-		local rest = arg1%arg2
-		-- instead returning the result, call the callback
-		handleresult(result, rest)
-	end
-	-- Call the divide function 
-	divide2fn(11,2,handleresultfn)
-	
-	-- The end
 	print("end")
 	os.exit()
 end
 
+-- ----------------------------------------------------------------------------
+-- bootstrap code
+-- ----------------------------------------------------------------------------
+
 sched.run(main)
 sched.loop()
+
